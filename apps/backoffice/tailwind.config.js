8d1ef6933bd2a8daf75b90a2bf2eec590d308c6a
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-04-12 23:10:18 +0700

    chore: config tailwindcss

--- apps/backoffice/tailwind.config.js
+++ apps/backoffice/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
