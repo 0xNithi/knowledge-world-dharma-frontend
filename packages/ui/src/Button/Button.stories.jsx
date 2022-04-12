e7a359e1ea4e58644be59e034a01db8e3b772a86
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-04-12 23:03:53 +0700

    chore: remove comment

--- packages/ui/src/Button/Button.stories.jsx
+++ packages/ui/src/Button/Button.stories.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Button from '.';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'General/Button',
   component: Button,
@@ -13,7 +12,6 @@ function Template(args) {
 }
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   color: 'primary',
   children: 'Button',
