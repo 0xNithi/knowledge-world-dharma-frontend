f58da9dbe55f4303af53aa818dcee485e19035b3
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-04-13 15:17:10 +0700

    feat: add icons

---
+++ packages/ui/src/Icon/index.js
@@ -0,0 +1,4 @@
+export { default as Annotation } from './Annotation';
+export { default as ChatAlt2 } from './ChatAlt2';
+export { default as Users } from './Users';
+export { default as ViewBoards } from './ViewBoards';
--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,3 +1,8 @@
+// General
 export { default as Button } from './Button';
 
+// Data Input
 export { default as Input } from './Input';
+
+// Icon
+export * from './Icon';
