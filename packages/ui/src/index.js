bb5b2c08bf433a8867ebdc0ba313ea44703c4044
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-04-13 16:56:07 +0700

    feat: add context, hooks and icon

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,8 +1,12 @@
 // General
-export { default as Button } from './Button';
+export { default as Button } from './components/Button';
+export * from './components/Icon';
 
 // Data Input
-export { default as Input } from './Input';
+export { default as Input } from './components/Input';
 
-// Icon
-export * from './Icon';
+// Context
+export * from './context/ThemeProvider';
+
+// Hook
+export { default as useTheme } from './hooks/useTheme';
