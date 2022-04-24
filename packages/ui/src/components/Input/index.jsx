3b6a68498ef82b61e2b2302685f996cadf4dc140
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-04-24 22:02:18 +0700

    fix: forwardRef components

--- packages/ui/src/components/Input/index.jsx
+++ packages/ui/src/components/Input/index.jsx
@@ -3,24 +3,32 @@ import PropTypes from 'prop-types';
 
 import './Input.css';
 
-function Input({ id, type, label, size, className, ...props }) {
-  const classes = ['input', `input-${size}`];
+const Input = React.forwardRef(
+  ({ id, type, label, size, className, ...props }, ref) => {
+    const classes = ['input', `input-${size}`];
 
-  if (className) {
-    classes.push(className);
-  }
+    if (className) {
+      classes.push(className);
+    }
 
-  return (
-    <>
-      {label && (
-        <label htmlFor={id} className={`input-label input-label-${size}`}>
-          {label}
-        </label>
-      )}
-      <input id={id} type={type} className={classes.join(' ')} {...props} />
-    </>
-  );
-}
+    return (
+      <>
+        {label && (
+          <label htmlFor={id} className={`input-label input-label-${size}`}>
+            {label}
+          </label>
+        )}
+        <input
+          id={id}
+          type={type}
+          className={classes.join(' ')}
+          ref={ref}
+          {...props}
+        />
+      </>
+    );
+  },
+);
 
 Input.propTypes = {
   type: PropTypes.oneOf(['text', 'email', 'password']),
