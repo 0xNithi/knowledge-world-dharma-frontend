bfdcd8d1c57ad8df1ee0c90a7e700395baad84ee
Nithi Nomprawat <nithi.boss157@gmail.com>
2022-04-26 05:46:50 +0700

    feat: add CRUD user

--- apps/backoffice/src/state/users/hook.js
+++ apps/backoffice/src/state/users/hook.js
@@ -1,7 +1,15 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchUsers } from '.';
+import {
+  initialize as initializeAction,
+  fetchCreateUser,
+  fetchDeleteUser,
+  fetchUser,
+  fetchUsers,
+  fetchUpdateUser,
+  fetchBanUser,
+} from '.';
 
 export const useFetchUsers = () => {
   const dispatch = useDispatch();
@@ -12,6 +20,62 @@ export const useFetchUsers = () => {
 };
 
 export const useUsers = () => {
+  const dispatch = useDispatch();
+  const accessToken = useSelector((state) => state.user.accessToken);
   const { users, isLoading, error } = useSelector((state) => state.users);
-  return { users, isLoading, error };
+
+  const handleView = useCallback(
+    ({ slug }) => {
+      const userFound = users.find((user) => user.id.toString() === slug);
+
+      if (!userFound) {
+        dispatch(fetchUser({ slug }));
+      }
+      return userFound;
+    },
+    [users, dispatch],
+  );
+
+  const handleCreate = useCallback(
+    ({ data }) => {
+      dispatch(fetchCreateUser({ data }));
+    },
+    [dispatch],
+  );
+
+  const handleUpdate = useCallback(
+    ({ slug, data }) => {
+      dispatch(fetchUpdateUser({ slug, accessToken, data }));
+    },
+    [accessToken, dispatch],
+  );
+
+  const handleDelete = useCallback(
+    ({ slug }) => {
+      dispatch(fetchDeleteUser({ slug, accessToken }));
+    },
+    [accessToken, dispatch],
+  );
+
+  const handleBan = useCallback(
+    ({ slug }) => {
+      dispatch(fetchBanUser({ slug, accessToken }));
+    },
+    [accessToken, dispatch],
+  );
+
+  useEffect(() => {
+    dispatch(initializeAction());
+  }, [dispatch]);
+
+  return {
+    users,
+    isLoading,
+    error,
+    handleView,
+    handleCreate,
+    handleUpdate,
+    handleDelete,
+    handleBan,
+  };
 };
--- apps/backoffice/src/state/users/index.js
+++ apps/backoffice/src/state/users/index.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import axios from 'axios';
+import axios from 'axios';
 
 import UserAPI from '../../api/user';
 
@@ -14,23 +14,179 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
   return response.data;
 });
 
+export const fetchUser = createAsyncThunk(
+  'users/fetchUser',
+  async ({ slug }, { rejectWithValue }) => {
+    try {
+      const response = await UserAPI.all();
+      if (response.data.find((user) => user.id.toString() === slug)) {
+        return response.data;
+      }
+      throw new Error('User not found!');
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.response.data);
+      }
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+      throw error;
+    }
+  },
+);
+
+export const fetchCreateUser = createAsyncThunk(
+  'users/fetchCreateUser',
+  async ({ data }, { rejectWithValue }) => {
+    try {
+      const response = await UserAPI.register({ data });
+      const user = await UserAPI.profile({ accessToken: response.data });
+      return user.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.response.data);
+      }
+      throw error;
+    }
+  },
+);
+
+export const fetchUpdateUser = createAsyncThunk(
+  'users/fetchUpdateUser',
+  async ({ slug, accessToken, data }, { rejectWithValue }) => {
+    try {
+      await UserAPI.update({ slug, accessToken, data });
+      return { slug, data };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.response.data);
+      }
+      throw error;
+    }
+  },
+);
+
+export const fetchDeleteUser = createAsyncThunk(
+  'users/fetchDeleteUser',
+  async ({ slug, accessToken }, { rejectWithValue }) => {
+    try {
+      await UserAPI.delete({ slug, accessToken });
+      return slug;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.response.data);
+      }
+      throw error;
+    }
+  },
+);
+
+export const fetchBanUser = createAsyncThunk(
+  'users/fetchBanUser',
+  async ({ slug, accessToken }, { rejectWithValue }) => {
+    try {
+      const response = await UserAPI.ban({ slug, accessToken });
+      console.log({
+        slug,
+        data: { banned: response.data.toLowerCase().includes('true') },
+      });
+      return {
+        slug,
+        data: { banned: response.data.toLowerCase().includes('true') },
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.response.data);
+      }
+      throw error;
+    }
+  },
+);
+
 export const usersSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {},
+  reducers: {
+    initialize: (state) => {
+      return { ...initialState, users: state.users };
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchUsers.pending, (state) => {
         state.isLoading = true;
       })
+      .addCase(fetchUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchCreateUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchUpdateUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchDeleteUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchBanUser.pending, (state) => {
+        state.isLoading = true;
+      })
       .addCase(fetchUsers.fulfilled, (state, { payload }) => {
         state.users = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchUser.fulfilled, (state, { payload }) => {
+        state.users = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchCreateUser.fulfilled, (state, { payload }) => {
+        state.users.push(payload);
+        state.isLoading = false;
+      })
+      .addCase(fetchUpdateUser.fulfilled, (state, { payload }) => {
+        state.users = state.users.map((data) => {
+          if (data.id.toString() === payload.slug) {
+            return { ...data, ...payload.data };
+          }
+          return data;
+        });
+      })
+      .addCase(fetchDeleteUser.fulfilled, (state, { payload }) => {
+        state.users = state.users.filter((user) => user.id !== payload);
+        state.isLoading = false;
+      })
+      .addCase(fetchBanUser.fulfilled, (state, { payload }) => {
+        state.users = state.users.map((data) => {
+          if (data.id.toString() === payload.slug) {
+            return { ...data, ...payload.data };
+          }
+          return data;
+        });
+      })
+      .addCase(fetchUser.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchCreateUser.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchUpdateUser.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchDeleteUser.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchBanUser.rejected, (state, { payload }) => {
+        state.error = payload;
         state.isLoading = false;
       });
   },
 });
 
 // Actions
-// export const { resetError, toggleTheme } = userSlice.actions;
+export const { initialize } = usersSlice.actions;
 
 export default usersSlice.reducer;
